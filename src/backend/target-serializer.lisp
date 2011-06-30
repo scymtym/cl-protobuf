a3421a6031497558d441872c402e50e1f29a101b
Jan Moringen <jmoringe@azurit>
2011-06-30 14:12:54 +0200

    Use with-descriptor-fields in src/backend/target-serializer.lisp * src/backend/target-serializer.lisp   (emit message-desc target-packed-size): use `with-descriptor-fields'   (emit field-desc target-packed-size): likewise   (emit message-desc target-serializer): likewise   (emit field-desc target-serializer): likewise   (emit message-desc target-deserializer): likewise   (emit field-desc target-deserializer): likewise

--- src/backend/target-serializer.lisp
+++ src/backend/target-serializer.lisp
@@ -50,41 +50,32 @@ obtained by parsing the binary output of protoc.")
   (:documentation
    "Target class for packed-size target."))
 
-(defmethod emit ((node   pb::message-desc)
+(defmethod emit ((node   message-desc)
 		 (target target-packed-size)
 		 &key)
   "Generate code for the `packed-size' method."
   (with-emit-symbols
-    (bind (((:accessors-r/o
-	     (name   pb::message-desc-name)
-	     (nested pb::message-desc-nested-type)
-	     (fields pb::message-desc-field)) node)
-	   (name1 (intern* (make-lisp-class-name name parent))))
-      (map 'nil #'recur nested)
-      (eval (generate-packed-size-method
-	     name1 (map 'list #'recur fields))))))
+    (with-descriptor-fields (node message-desc)
+      (bind ((name1 (intern* (make-lisp-class-name name parent))))
+	(map 'nil #'recur nested-type)
+	(eval (generate-packed-size-method
+	       name1 (map 'list #'recur field)))))))
 
-(defmethod emit ((node   pb::field-desc)
+(defmethod emit ((node   field-desc)
 		 (target target-packed-size)
 		 &key)
   "Generate code to compute the packed size of a single slot."
   (with-emit-symbols
-    (bind (((:accessors-r/o
-	     (name      pb::field-desc-name)
-	     (type      pb::field-desc-type)
-	     (type-name pb::field-desc-type-name)
-	     (number    pb::field-desc-number)
-	     (label     pb::field-desc-label)
-	     (options   pb::field-desc-options)) node)
-	   (name1     (intern* (make-lisp-slot-name name)))
-	   (type1     (make-lisp-slot-type type type-name package))
-	   (repeated? (eq label :repeated))
-	   (packed?   (when options
-			(pb::field-options-packed options))))
-      #'(lambda (object-var)
-	  (generate-slot-packed-size name1 type1 number object-var
-				     :repeated?  repeated?
-				     :packed?    packed?)))))
+    (with-descriptor-fields (node field-desc)
+     (bind ((name1     (intern* (make-lisp-slot-name name)))
+	    (type1     (make-lisp-slot-type type type-name package))
+	    (repeated? (eq label :repeated))
+	    (packed?   (when options
+			 (pb::field-options-packed options))))
+       #'(lambda (object-var)
+	   (generate-slot-packed-size name1 type1 number object-var
+				      :repeated?  repeated?
+				      :packed?    packed?))))))
 
 
 ;;; Serializer
@@ -99,41 +90,32 @@ obtained by parsing the binary output of protoc.")
   (:documentation
    "Target class for serializer target."))
 
-(defmethod emit ((node   pb::message-desc)
+(defmethod emit ((node   message-desc)
 		 (target target-serializer)
 		 &key)
   "Generate code for the `pack' method."
   (with-emit-symbols
-    (bind (((:accessors-r/o
-	     (name   pb::message-desc-name)
-	     (nested pb::message-desc-nested-type)
-	     (fields pb::message-desc-field)) node)
-	   (name1 (intern* (make-lisp-class-name name parent))))
-      (map 'nil #'recur nested)
-      (eval (generate-pack-method name1 (map 'list #'recur fields))))))
+    (with-descriptor-fields (node message-desc)
+      (bind ((name1 (intern* (make-lisp-class-name name parent))))
+	(map 'nil #'recur nested-type)
+	(eval (generate-pack-method name1 (map 'list #'recur field)))))))
 
-(defmethod emit ((node   pb::field-desc)
+(defmethod emit ((node   field-desc)
 		 (target target-serializer)
 		 &key)
   "Generate code to pack a single slot."
   (with-emit-symbols
-    (bind (((:accessors-r/o
-	     (name      pb::field-desc-name)
-	     (number    pb::field-desc-number)
-	     (type      pb::field-desc-type)
-	     (type-name pb::field-desc-type-name)
-	     (label     pb::field-desc-label)
-	     (options   pb::field-desc-options)) node)
-	   (name1     (intern* (make-lisp-slot-name name)))
-	   (type1     (make-lisp-slot-type type type-name package))
-	   (repeated? (eq label :repeated))
-	   (packed?   (when options
-			(pb::field-options-packed options))))
-      #'(lambda (buffer-var offset-var object-var)
-	  (generate-slot-packer
-	   type1 name1 number buffer-var offset-var object-var
-	   :repeated? repeated?
-	   :packed?   packed?)))))
+    (with-descriptor-fields (node field-desc)
+      (bind ((name1     (intern* (make-lisp-slot-name name)))
+	     (type1     (make-lisp-slot-type type type-name package))
+	     (repeated? (eq label :repeated))
+	     (packed?   (when options
+			  (pb::field-options-packed options))))
+	#'(lambda (buffer-var offset-var object-var)
+	    (generate-slot-packer
+	     type1 name1 number buffer-var offset-var object-var
+	     :repeated? repeated?
+	     :packed?   packed?))))))
 
 
 ;;; Deserializer
@@ -148,47 +130,38 @@ obtained by parsing the binary output of protoc.")
   (:documentation
    "Target class for deserializer target."))
 
-(defmethod emit ((node   pb::message-desc)
+(defmethod emit ((node   message-desc)
 		 (target target-deserializer)
 		 &key)
   "Generate code for the `unpack' method."
   (with-emit-symbols
-    (bind (((:accessors-r/o
-	     (name   pb::message-desc-name)
-	     (nested pb::message-desc-nested-type)
-	     (fields pb::message-desc-field)) node)
-	   (name1 (intern* (make-lisp-class-name name parent))))
-      (map 'nil #'recur nested)
-      (eval (generate-unpack-method name1 (map 'list #'recur fields))))))
+    (with-descriptor-fields (node message-desc)
+     (bind ((name1 (intern* (make-lisp-class-name name parent))))
+       (map 'nil #'recur nested-type)
+       (eval (generate-unpack-method name1 (map 'list #'recur field)))))))
 
-(defmethod emit ((node   pb::field-desc)
+(defmethod emit ((node   field-desc)
 		 (target target-deserializer)
 		 &key)
   "Generate code to unpack a single slot"
   (with-emit-symbols
-    (bind (((:accessors-r/o
-	     (name      pb::field-desc-name)
-	     (number    pb::field-desc-number)
-	     (type      pb::field-desc-type)
-	     (type-name pb::field-desc-type-name)
-	     (label     pb::field-desc-label)
-	     (options   pb::field-desc-options)) node)
-	   (name1     (intern* (make-lisp-slot-name name)))
-	   (type1     (make-lisp-slot-type type type-name package))
-	   (repeated? (eq label :repeated))
-	   (packed?   (when options
-			(pb::field-options-packed options)))
-	   (desired-wire-type (proto-type->wire-type type repeated? packed?)))
-      #'(lambda (read-wire-type-var buffer-form offset-form object-form)
-	  (values
-	   number
-	   `((unless (= ,read-wire-type-var ,desired-wire-type) ;; TODO move to generator-code
-	       (error 'pb:unexpected-wire-type
-		      :field         ',name1
-		      :offset        ,offset-form
-		      :expected-type ,desired-wire-type
-		      :found-type    ,read-wire-type-var))
-	     ,@(generate-slot-unpacker
-		type1 name1 buffer-form offset-form object-form
-		:repeated? repeated?
-		:packed?   packed?)))))))
+    (with-descriptor-fields (node field-desc)
+      (bind ((name1     (intern* (make-lisp-slot-name name)))
+	     (type1     (make-lisp-slot-type type type-name package))
+	     (repeated? (eq label :repeated))
+	     (packed?   (when options
+			  (pb::field-options-packed options)))
+	     (desired-wire-type (proto-type->wire-type type repeated? packed?)))
+	#'(lambda (read-wire-type-var buffer-form offset-form object-form)
+	    (values
+	     number
+	     `((unless (= ,read-wire-type-var ,desired-wire-type) ;; TODO move to generator-code
+		 (error 'unexpected-wire-type
+			:field         ',name1
+			:offset        ,offset-form
+			:expected-type ,desired-wire-type
+			:found-type    ,read-wire-type-var))
+	       ,@(generate-slot-unpacker
+		  type1 name1 buffer-form offset-form object-form
+		  :repeated? repeated?
+		  :packed?   packed?))))))))
